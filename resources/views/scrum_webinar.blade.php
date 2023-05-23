fff4b83bb0d13592b23fc63f5f7b1962d5dc1448
Promise <promiseezema11@gmail.com>
2023-05-23 23:21:44 +0100

    updated

--- resources/views/scrum_webinar.blade.php
+++ resources/views/scrum_webinar.blade.php
@@ -1,93 +1,195 @@
-@extends('layouts.frontend')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('navbar')
-@includeIf('layouts.navbar')
-@endsection
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{config('app.name')}} - Scrum Webinar</title>
+    <!-- fav icon -->
+    <link rel="shortcut icon" type="image/x-icon" href="{{URL::asset('images/favicon.png')}}" />
 
-@section('page-content')
-<div class="page-title-area">
-    <div class="container">
-        <div class="page-title-content">
-            <h2>Scrum Webinar</h2>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li class="active">Scrum Webinar</li>
-            </ul>
+    <link rel="stylesheet" href="assets/css/plugins/fontawesome-5.css">
+    <link rel="stylesheet" href="assets/css/vendor/swiper.css">
+    <link rel="stylesheet" href="assets/css/vendor/animate.min.css">
+    <link rel="stylesheet" href="assets/css/plugins/unicons.css">
+    <link rel="stylesheet" href="assets/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body class="onepage photographer-home">
+    <!-- header style two -->
+    <header class="header-one header--sticky">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-xl-2 col-lg-6 col-md-6 col-sm-6 col-6" style="margin-top: 1rem">
+                    <a href="/" class="thumbnail">
+                        <img src="{{URL::asset('images/logo.png')}}" alt="Ivavtech" style="width: 80px; height: 75px;">
+                    </a>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-
-<div class="privecy-area ptb-100">
-    <div class="container">
-        <h2>The new Tech Gold is here and Scrum is the new Gold.</h2>
-        <p>If you're willing to become a Scrum Master or a Product Owner but you don't know how to get started, this is for you.</p>
-        <div class="services-details-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="services-details-text">
-                            <h2><a class="box-btn signup-btn" href="#">Click Here To Register For The Free Webinar</a></h2>
-                        </div>
+    </header>
+    <!-- rts project area start -->
+    <div class="rts-project-area rts-section-gapBottom" id="portfolio">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="h2-title-area">
+                        <h2 class="title">
+                            The new Tech Gold is here and Scrum is the new Gold.
+                        </h2>
+                        <p style="font-size: 16px; color: #000">If you're willing to become a Scrum Master or a Product Owner but you don't know how to get started, this is for you.</p>
+                        <a href="#" class="rts-btn btn-main-2 mb-5">Click Here To Register For The Free Webinar</a>
+                        <p style="font-size: 16px; color: #000">And if you’re just hearing about Scrum, in simple terms, Scrum is a lucrative tech area of interest that helps teams and organizations produce maximum value while working on different projects.</p>
                     </div>
                 </div>
             </div>
         </div>
-        <p>And if you’re just hearing about Scrum, in simple terms, Scrum is a lucrative tech area of interest that helps teams and organizations produce maximum value while working on different projects.</p>
-        <p>Hello, I'm Oluwaseun</p>
-        <p>In 2015, I graduated with a degree in Geography but the first job opportunity I had after NYSC was in Digital Marketing.</p>
-        <p>To cut the long story short, I only practiced as a Digital Marketing professional until around mid- 2022 when I joined my current company as a Marketing Manager and through this new job opportunity, my aspirations of working alongside highly technical teams started becoming a reality.</p>
-        <p>Is Digital Marketing not full of opportunities?</p>
-        <p>Of course, it is! I knew Digital Marketing is very lucrative but Scrum and Product Ownership have risen to become the new 'oil' and experts in these areas are highly sought after.</p>
-        <p>But I had one problem...</p>
-        <p>I wasn't interested in coding. While I believe knowing the basics is not out of place, I knew it wasn't a thing I would love to do in the long term.</p>
-        <p>Thankfully, I met my mentor in this company and he introduced me to this profitable IT field (Scrum) I never knew existed.</p>
-        <p>For me, the distinctive approach of Scrum to actualizing both IT-related and non IT-related projects caught my attention.</p>
-        <p>Not only that, with many limitations that come with planning, executing and managing a project, Scrum has proven to be the most appropriate project management framework adopted by more than 70% tech startups and top IT firms in the world.</p>
-        <p>So, why wouldn't I embrace this rare privilege?</p>
-        <p>And since I started my journey in Scrum, I've never looked back because I've had no reason to regret.</p>
-        <h2 style="padding-top: 1.5rem;">Now Your Turn</h2>
-        <p>I know you've read a lot and watched several videos about how to get started but I'm a living proof that you can start your journey in Scrum without any tech related experience and my mentor also wants to show you in practical, how you can do that.</p>
-        <p>That's why he has organized a free webinar where he will reveal to you exactly how to transition smoothly into Scrum or Product Ownership (which some also call Product Management).</p>
-        <p>In this free webinar you will discover:</p>
-        <ul>
-            <li><p>1. How to become a qualified Scrum Master or Product Owner, even without a tech experience.</p></li>
-            <li><p>2. How he landed his first job offer and how you can earn your first Scrum master position.</p></li>
-            <li><p>3. What your CV should look like to make notable international companies develop interest in working with you, even as a newbie.</p></li>
-            <li><p>4. What the exact duties of a Scrum Master and a Product Owner.</p></li>
-            <li><p>5. The distinction between a Scrum Master and a Project Manager.</p></li>
-            <li><p>6. How much a beginner Scrum Master or Product Owner can make.</p></li>
-            <li><p>7. What varieties of job opportunities are available to Scrum Masters and Product Owners.</p></li>
-            <li><p>8. What certifications are available and which is the best.</p></li>
-        </ul>
-        <p>Are you ready to rewrite your career story?</p>
-        <div class="services-details-area">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="services-details-text">
-                            <h2><a class="box-btn signup-btn" href="#">Click Here To Register For The Free Webinar</a></h2>
-                        </div>
+    </div>
+    <!-- rts project area end -->
+
+    <!-- rts banenr area start -->
+    <div class="rts-banner-area bannerdd-two" id="home">
+        <div class="container">
+            <div class="row g-5">
+                <div class="col-lg-5">
+                    <div class="thumbnail text-anim">
+                        <img class="banner-thumb-2" src="assets/images/seun.jpeg" alt="Oluwaseun Picture">
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="home-2-banner-content mb--25">
+                        <span class="sub-title quote-2">
+                            Hello, I'm Oluwaseun
+                        </span>
+                        <h1 class="title quote">
+                            In 2015, I graduated with a degree in Geography but the first job opportunity I had after NYSC was in Digital Marketing.
+                        </h1>
+                        <p class="banner-disc text-anim">
+                            To cut the long story short, I only practiced as a Digital Marketing professional until around mid- 2022 when I joined my current company as a Marketing Manager and through this new job opportunity, my aspirations of working alongside highly technical teams started becoming a reality.
+                        </p>
+                        <p class="banner-disc text-anim">
+                            Is Digital Marketing not full of opportunities?
+                        </p>
+                        <p class="banner-disc text-anim">
+                            Of course, it is! I knew Digital Marketing is very lucrative but Scrum and Product Ownership have risen to become the new 'oil' and experts in these areas are highly sought after.
+                        </p>
+                        <p class="banner-disc text-anim">
+                            But I had one problem..
+                        </p>
+                        <p class="banner-disc text-anim">I wasn't interested in coding. While I believe knowing the basics is not out of place, I knew it wasn't a thing I would love to do in the long term.</p>
+                        <p class="banner-disc text-anim">Thankfully, I met my mentor in this company and he introduced me to this profitable IT field (Scrum) I never knew existed.</p>
+                        <p class="banner-disc text-anim">For me, the distinctive approach of Scrum to actualizing both IT-related and non IT-related projects caught my attention.</p>
+                        <p class="banner-disc text-anim">Not only that, with many limitations that come with planning, executing and managing a project, Scrum has proven to be the most appropriate project management framework adopted by more than 70% tech startups and top IT firms in the world.</p>
+                        <p class="banner-disc text-anim">So, why wouldn't I embrace this rare privilege?</p>
+                        <p class="banner-disc text-anim">And since I started my journey in Scrum, I've never looked back because I've had no reason to regret.</p>
                     </div>
                 </div>
             </div>
         </div>
-        <h2 style="padding-top: 1.5rem;">Video Transcript</h2>
-        <p>This special announcement is for anyone interested in Scrum, the non-coding IT field that's taking the industry by storm!</p>
-        <p>Have you have been looking for how to build a career in Scrum or become a Product Owner?</p>
-        <p>And if you're hearing about this for the first time and it sounds like a career path you may love to explore, then listen carefully.</p>
-        <p>UK-based certified Scrum Master and Agile Coach Adedayo Falusi, is offering a free webinar where he will share his insider knowledge on how to break into this highly lucrative field - even if you have no technical experience!</p>
-        <p>He will also show you:</p>
-        <ul>
-            <li><p>1. How you he landed his first job and how you can earn your first Scrum master position.</p></li>
-            <li><p>2 .What your CV should look like to make notable international companies develop interest in working with you, even as a newbie.</p></li>
-            <li><p>3. What the exact duties of a Scrum Master and a Product Owner.</p></li>
-            <li><p>4. How much a beginner Scrum Master or Product Owner can make.</p></li>
-            <li><p>5. What varieties of job opportunities are available to Scrum Masters and Product Owners.</p></li>
-        </ul>
-        <p>And many more.</p>
-        <p>If this sounds like a game changer for you,</p>
-        <p>Then click the link on this page to read the inspiring story of Oluwaseun who successfully entered the world of Scrum with no prior tech experience and see how you can also get started on this journey.</p>
-        <p>See you soon.</p>
     </div>
-</div>
-@endsection
+    <!-- rts banenr area end -->
+
+    <!-- rts about kiills area -->
+    <div class="rts-skill-area about-h3 rts-section-gapBottom" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="tab-area-main-wrapper text-anim">
+
+                        <!-- tab area start -->
+                        <ul class="nav nav-tabs" id="myTab" role="tablist">
+                            <li class="nav-item" role="presentation">
+                                <button class="nav-link active" id="profile-tab" data-bs-toggle="tab" data-bs-target="#yourturn" type="button" role="tab" aria-controls="yourturn" aria-selected="true">Now Your Turn</button>
+                            </li>
+                        </ul>
+                        <!-- tab area End -->
+
+                        <!-- tab content area start -->
+                        <div class="tab-content" id="myTabContent">
+                            <div class="tab-pane fade show active" id="yourturn" role="tabpanel" aria-labelledby="yourturn-tab">
+                                <div class="single-tab-wrapper">
+                                    <!-- single tab content -->
+                                    <div class="row align-items-center">
+                                        <div class="col-12 pb_sm--20">
+                                            <div class="content-main">
+                                                <h2 class="title">
+                                                    Taking Dynamic Photos is all
+                                                    about my Passion
+                                                </h2>
+                                                <p class="about-disc">I know you've read a lot and watched several videos about how to get started but I'm a living proof that you can start your journey in Scrum without any tech related experience and my mentor also wants to show you in practical, how you can do that.</p>
+                                                <p class="about-disc">That's why he has organized a free webinar where he will reveal to you exactly how to transition smoothly into Scrum or Product Ownership (which some also call Product Management).</p>
+                                                <p class="about-disc">In this free webinar you will discover:</p>
+                                                <ul class="about-disc">
+                                                    <li>How to become a qualified Scrum Master or Product Owner, even without a tech experience.</li>
+                                                    <li>How he landed his first job offer and how you can earn your first Scrum master position.</li>
+                                                    <li>What your CV should look like to make notable international companies develop interest in working with you, even as a newbie.</li>
+                                                    <li>What the exact duties of a Scrum Master and a Product Owner.</li>
+                                                    <li>The distinction between a Scrum Master and a Project Manager.</li>
+                                                    <li>How much a beginner Scrum Master or Product Owner can make.</li>
+                                                    <li>What varieties of job opportunities are available to Scrum Masters and Product Owners.</li>
+                                                    <li>What certifications are available and which is the best.</li>
+                                                </ul>
+                                                <a href="#" class="rts-btn btn-main-2 mb-5">Click Here To Register For The Free Webinar</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <!-- single tab content End -->
+                                </div>
+                            </div>
+                        </div>
+                        <!-- tab content area End -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts about kiills area End -->
+
+    <!-- progress Back to top -->
+    <div class="progress-wrap">
+        <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+            <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+        </svg>
+    </div>
+    <!-- progress Back to top End -->
+
+    <!-- scripts -->
+
+    <script src="assets/js/vendor/jquery.min.js"></script>
+    <script src="assets/js/vendor/jquery.nav.js"></script>
+
+    <script src="assets/js/vendor/jquery-ui.min.js"></script>
+
+    <script src="assets/js/vendor/waw.js"></script>
+    <script src="assets/js/vendor/waypoint.js"></script>
+
+    <script src="assets/js/plugins/splittext.js"></script>
+    <script src="assets/js/plugins/gsap.min.js"></script>
+    <script src="assets/js/plugins/scrolltigger.js"></script>
+
+    <script src="assets/js/plugins/counter-up.js"></script>
+    <script src="assets/js/plugins/smoothscrool.js"></script>
+    <script src="assets/js/plugins/scroll-out.js"></script>
+    <script src="assets/js/plugins/marquery.js"></script>
+    <script src="assets/js/plugins/parallax.js"></script>
+
+    <script src="assets/js/plugins/contact-form.js"></script>
+
+    <!-- <script src="assets/js/vendor/scroll-smoother.js"></script> -->
+
+    <script src="assets/js/vendor/swiper.js"></script>
+    <script src="assets/js/vendor/sal.min.js"></script>
+
+    <!-- for smoothj scroll -->
+    <script src="assets/js/vendor/tweenmax.js"></script>
+
+    <script src="assets/js/vendor/bootstrap.min.js"></script>
+
+
+    <!-- main js hear -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
